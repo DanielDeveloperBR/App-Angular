5d200514ffc56f1b4a6e71542aa31ee539ad77a0
Daniel <115842725+DanielDeveloperBR@users.noreply.github.com>
2023-08-25 21:37:16 -0300

    Aula 1 25/08
    Primeiro commit do Angula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { BotaoComponent } from './components/botao/botao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent,
+    BotaoComponent
   ],
   imports: [
     BrowserModule,
